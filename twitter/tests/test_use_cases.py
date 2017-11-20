10fb7fc67662bab2446ee1b777ddd5b6a0189a50
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-20 17:18:14 -0200

    Implement use case to fetch and save twitter user

--- twitter/tests/test_use_cases.py
+++ twitter/tests/test_use_cases.py
@@ -1,8 +1,61 @@
-from django.test import TestCase
-from django.conf import settings
+from unittest.mock import patch
 
-from twitter.use_cases import FetchTweetsUseCase, FetchUserUseCase
+from django.test import TestCase
+
+from twitter.models import TwitterUser
+from twitter.tests.specs import dog_rates_response
+from twitter.use_cases import FetchUserUseCase
 
 
 class FetchUserUseCaseTest(TestCase):
-    pass
+
+    @patch('twitter.use_cases.UsersLookupClient.__call__')
+    @patch('twitter.use_cases.UsersLookupClient.__init__')
+    def test_success_saves_user(self, mocked_client, mocked_client_call):
+        mocked_client.return_value = None
+        mocked_client_call.return_value = {
+            'status': 200, 'data': dog_rates_response}
+        users_count = TwitterUser.objects.count()
+
+        use_case = FetchUserUseCase()
+        result = use_case.execute('dog_rates')
+
+        params = {'params': {'screen_name': 'dog_rates'}}
+        mocked_client.assert_called_once_with(action_params=params)
+        mocked_client_call.assert_called_once_with()
+        self.assertEqual(users_count + 1, TwitterUser.objects.count())
+        self.assertEqual('Success! User added to feed.', result)
+
+    @patch('twitter.use_cases.UsersLookupClient.__call__')
+    @patch('twitter.use_cases.UsersLookupClient.__init__')
+    def test_user_not_found_returns_expected_message(self, mocked_client, mocked_client_call):
+        mocked_client.return_value = None
+        mocked_client_call.return_value = {
+            'status': 404, 'data': {}}
+
+        use_case = FetchUserUseCase()
+        result = use_case.execute('not_ratting_dogs')
+
+        params = {'params': {'screen_name': 'not_ratting_dogs'}}
+        mocked_client.assert_called_once_with(action_params=params)
+        mocked_client_call.assert_called_once_with()
+        self.assertEqual(
+            'There is no Twitter user with the given username.',
+            result
+        )
+
+    @patch('twitter.use_cases.UsersLookupClient.__call__')
+    @patch('twitter.use_cases.UsersLookupClient.__init__')
+    def test_any_other_error_returns_unexpected_error_message(self, mocked_client, mocked_client_call):
+        mocked_client.return_value = None
+        mocked_client_call.return_value = {
+            'status': 500, 'data': {}}
+
+        use_case = FetchUserUseCase()
+        result = use_case.execute('dog_rates')
+
+        params = {'params': {'screen_name': 'dog_rates'}}
+        mocked_client.assert_called_once_with(action_params=params)
+        mocked_client_call.assert_called_once_with()
+        message = 'Something went wrong and the user could not be added. Please try againg later or contact our support team.'
+        self.assertEqual(message, result)
--- twitter/use_cases.py
+++ twitter/use_cases.py
@@ -1,13 +1,21 @@
-from tapioca_twitter import Twitter
-from django.conf import settings
+from twitter.clients import UsersLookupClient
+from twitter.serializers import TwitterUserCreateSerializer
 
 
-class FetchTweetsUseCase(object):
+class FetchUserUseCase(object):
 
-    def __init__(self):
-        self.twitter = Twitter(
-            api_key=settings.TWITTER_KEY,
-            api_secret=settings.TWITTER_SECRET_KEY,
-            access_token=settings.TWITTER_TOKEN,
-            access_token_secret=settings.TWITTER_SECRET_TOKEN,
-        )
+    def execute(self, twitter_username):
+        message = 'Something went wrong and the user could not be added. Please try againg later or contact our support team.'
+        params = {'params': {'screen_name': twitter_username}}
+        client = UsersLookupClient(action_params=params)
+        response = client()
+        status = response['status']
+        data = response['data']
+        if status == 200:
+            serializer = TwitterUserCreateSerializer(data=data[0])
+            if serializer.is_valid():
+                serializer.save()
+                message = 'Success! User added to feed.'
+        elif status == 404:
+            message = 'There is no Twitter user with the given username.'
+        return message
