32d1526645ca1a3272b8bc4c62c30d6b0a0c2ca1
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-20 00:06:22 -0200

    Create twitter user and tweet models

--- twitter/models.py
+++ twitter/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class TwitterUser(models.Model):
+    twitter_id = models.BigIntegerField(_('Twitter Id'), unique=True)
+    screen_name = models.CharField(_('Twitter username'),
+                                   unique=True, max_length=255)
+    name = models.CharField(_('Name'), max_length=255)
+
+
+class Tweet(models.Model):
+    user = models.ForeignKey(
+        'twitter.TwitterUser',
+        verbose_name=_('User'),
+        on_delete=models.CASCADE
+    )
+    content = models.TextField(_('Content'))
+    posted_at = models.DateTimeField(_('Posted at'))
---
+++ twitter/tests/test_models.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.utils import timezone
+
+from model_mommy import mommy
+
+from twitter.models import Tweet, TwitterUser
+
+
+class TwitterUserModelTest(TestCase):
+
+    def test_create_instance(self):
+        user = TwitterUser.objects.create(
+            twitter_id=1234567890987654321,
+            screen_name='coolUser_name',
+            name='Even Cooler Name'
+        )
+        self.assertEqual(1234567890987654321, user.twitter_id)
+        self.assertEqual('coolUser_name', user.screen_name)
+        self.assertEqual('Even Cooler Name', user.name)
+
+
+class TweetModelTest(TestCase):
+
+    def test_create_instance(self):
+        user = mommy.make(TwitterUser)
+        now = timezone.now()
+        tweet = Tweet.objects.create(
+            user=user,
+            content='Awesome Tweet',
+            posted_at=now
+        )
+        self.assertEqual(user, tweet.user)
+        self.assertEqual('Awesome Tweet', tweet.content)
+        self.assertEqual(now, tweet.posted_at)
+
+    def test_deleting_user_deletes_tweet(self):
+        user = mommy.make(TwitterUser)
+        tweet = mommy.make(Tweet, user=user)
+        TwitterUser.objects.filter(id=user.id).delete()
+        self.assertRaises(Tweet.DoesNotExist, Tweet.objects.get, id=tweet.id)
