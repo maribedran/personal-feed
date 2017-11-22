51c70e647cd1b80ae7c575e63a81b6a2481a780f
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-22 18:34:28 -0200

    Refactor TwitterUser and Tweet models
    - Add owners field to TwitterUser with m2m to User
    - Add description field to TwitterUser
    - Add db indexes to both models
    - Add str methods to both models
    - Add verbose name to both model's Meta classes

--- twitter/models.py
+++ twitter/models.py
@@ -1,20 +1,54 @@
 from django.db import models
+from django.utils.text import Truncator
 from django.utils.translation import ugettext_lazy as _
 
 
 class TwitterUser(models.Model):
-    twitter_id = models.BigIntegerField(_('Twitter Id'), unique=True)
-    screen_name = models.CharField(_('Twitter username'),
-                                   unique=True, max_length=255)
+    owners = models.ManyToManyField(
+        'users.User',
+        verbose_name=_('Owner')
+    )
+    twitter_id = models.BigIntegerField(
+        _('Twitter Id'),
+        unique=True,
+        db_index=True
+    )
+    screen_name = models.CharField(
+        _('Twitter username'),
+        unique=True,
+        max_length=255,
+        db_index=True
+    )
     name = models.CharField(_('Name'), max_length=255)
+    description = models.TextField(_('Description'), blank=True, null=True)
+
+    def __str__(self):
+        return self.screen_name
+
+    class Meta:
+        verbose_name = _('Twitter User')
+        verbose_name_plural = _('Twitter Users')
 
 
 class Tweet(models.Model):
-    twitter_id = models.BigIntegerField(_('Twitter Id'), unique=True)
+    twitter_id = models.BigIntegerField(
+        _('Twitter Id'),
+        unique=True,
+        db_index=True
+    )
     user = models.ForeignKey(
         'twitter.TwitterUser',
         verbose_name=_('User'),
+        db_index=True,
         on_delete=models.CASCADE
     )
     text = models.TextField(_('Content'))
     created_at = models.DateTimeField(_('Posted at'))
+
+    def __str__(self):
+        return Truncator(self.text).chars(70)
+
+    class Meta:
+        verbose_name = _('Tweet')
+        verbose_name_plural = _('Tweets')
+        index_together = [['twitter_id', 'user']]
--- twitter/tests/test_models.py
+++ twitter/tests/test_models.py
@@ -9,14 +9,24 @@ from twitter.models import Tweet, TwitterUser
 class TwitterUserModelTest(TestCase):
 
     def test_create_instance(self):
-        user = TwitterUser.objects.create(
+        twitter_user = TwitterUser.objects.create(
             twitter_id=1234567890987654321,
             screen_name='coolUser_name',
             name='Even Cooler Name'
         )
-        self.assertEqual(1234567890987654321, user.twitter_id)
-        self.assertEqual('coolUser_name', user.screen_name)
-        self.assertEqual('Even Cooler Name', user.name)
+        self.assertEqual(1234567890987654321, twitter_user.twitter_id)
+        self.assertEqual('coolUser_name', twitter_user.screen_name)
+        self.assertEqual('Even Cooler Name', twitter_user.name)
+
+    def test_add_owner(self):
+        user = mommy.make('users.User')
+        twitter_user = mommy.make('twitter.TwitterUser')
+        twitter_user.owners.add(user)
+        self.assertEqual(user, twitter_user.owners.first())
+
+    def test_str_returns_screen_name(self):
+        twitter_user = mommy.make('twitter.TwitterUser', screen_name='some_name')
+        self.assertEqual('some_name', str(twitter_user))
 
 
 class TweetModelTest(TestCase):
@@ -40,3 +50,7 @@ class TweetModelTest(TestCase):
         tweet = mommy.make(Tweet, user=user)
         TwitterUser.objects.filter(id=user.id).delete()
         self.assertRaises(Tweet.DoesNotExist, Tweet.objects.get, id=tweet.id)
+
+    def test_str_returns_truncated_text(self):
+        tweet = mommy.make('twitter.Tweet', text='A' * 100)
+        self.assertEqual('%s...' % ('A' * 67), str(tweet))
