c2098b37e820f12f7323ef218cbefb116eb31dcd
Mariana Bedran Lesche <maribedran@gmail.com>
2017-11-23 01:13:26 -0200

    Add rest framework permission and pagination settings

--- personal_feed/settings/base.py
+++ personal_feed/settings/base.py
@@ -145,3 +145,12 @@ TWITTER_SECRET_TOKEN = config('TWITTER_SECRET_TOKEN')
 SOCIAL_AUTH_TWITTER_KEY = TWITTER_KEY
 SOCIAL_AUTH_TWITTER_SECRET = TWITTER_SECRET_KEY
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
+
+# Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+}
