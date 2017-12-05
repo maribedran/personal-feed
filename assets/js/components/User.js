f126bda7e8fb2745d47ae28ba3b463b54f3f8a45
Mariana Bedran Lesche <maribedran@gmail.com>
2017-12-05 01:58:34 -0200

    Add line break to user component

--- assets/js/components/User.js
+++ assets/js/components/User.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 
 const User = ({ user }) => {
     return (
-        <div><strong> {user.name}</strong> @{user.screen_name}</div>
+        <div>
+            <strong> {user.name}</strong><br />
+            @{user.screen_name}
+        </div>
     );
 };
 
